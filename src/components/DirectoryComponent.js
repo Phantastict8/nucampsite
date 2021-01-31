d3aaf3ffaa4f21f6229631c3b39edba6dc489aff
Peter Phan <peter_phan05@yahoo.com>
2021-01-31 14:52:47 -0800

    workshop2

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -7,32 +7,18 @@ import {
     CardBody,
     CardTitle,
 } from "reactstrap";
+import CampsiteInfo from "./CampsiteInfoComponents";
 
 class Directory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedCampsite: null
+            selectedCampsite: null,
         };
     }
 
     onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
-
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
+        this.setState({ selectedCampsite: campsite });
     }
 
     render() {
@@ -40,7 +26,11 @@ class Directory extends Component {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
                     <Card onClick={() => this.onCampsiteSelect(campsite)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImg
+                            width="100%"
+                            src={campsite.image}
+                            alt={campsite.name}
+                        />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
                         </CardImgOverlay>
@@ -54,11 +44,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+                <CampsiteInfo campsite = {this.state.selectedCampsite} />
             </div>
         );
     }
